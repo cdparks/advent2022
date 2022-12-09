d41fe4ee62e98072121f39d204b27e5c3b64a21e
Chris Parks <christopher.daniel.parks@gmail.com>
2022-12-09 13:10:51 -0800

    Use Vec2 in solution for day 8

--- src/Advent/Day08Spec.hs
+++ src/Advent/Day08Spec.hs
@@ -7,6 +7,7 @@ import Advent.Prelude
 import Advent.Input
 import Advent.Parse (Parser, digit, sepBy1, endOfLine)
 import qualified Data.HashMap.Strict as HashMap
+import Advent.Vec2 (Vec2(..))
 
 spec :: Spec
 spec = parsing parseGrid 8 $ do
@@ -21,15 +22,15 @@ spec = parsing parseGrid 8 $ do
 -- | Count trees that are visible from at least one edge
 part1 :: Grid -> Int
 part1 grid = flip execState 0 $
-  for_ (HashMap.toList grid) $ \((i, j), x) -> do
-    let vis = any (all (< x) . scan grid (i, j)) [minBound..]
+  for_ (HashMap.toList grid) $ \(pos, x) -> do
+    let vis = any (all (< x) . scan grid pos) [minBound..]
     when vis $ modify succ
 
 -- | Find the highest scenic score
 part2 :: Grid -> Int
 part2 grid = maximum $ do
-  ((i, j), x) <- HashMap.toList grid
-  pure $ product $ score grid (i, j) x <$> [minBound..]
+  (pos, x) <- HashMap.toList grid
+  pure $ product $ score grid pos x <$> [minBound..]
 
 -- | Cardinal directions
 data Direction = N | E | S | W
@@ -37,24 +38,24 @@ data Direction = N | E | S | W
 
 -- | Stream of all predecessors or successors of initial position in
 -- some direction
-scan :: Grid -> (Int, Int) -> Direction -> [Int]
-scan grid (i, j) = \case
-  N -> pick (preds  i) (repeat j)
-  E -> pick (repeat i) (succs  j)
-  S -> pick (succs  i) (repeat j)
-  W -> pick (repeat i) (preds  j)
+scan :: Grid -> Vec2 Int -> Direction -> [Int]
+scan grid (Vec2 j i) = \case
+  N -> pick (repeat j) (preds  i)
+  E -> pick (succs  j) (repeat i)
+  S -> pick (repeat j) (succs  i)
+  W -> pick (preds  j) (repeat i)
  where
   succs x = [x + 1 .. ]
   preds x = [x - 1, x - 2 .. ]
-  pick xs = takeWhileJust . fmap (`HashMap.lookup` grid) . zip xs
+  pick xs = takeWhileJust . fmap (`HashMap.lookup` grid) . zipWith Vec2 xs
 
 -- | Score is the number of sequential trees (<) x, plus one for the
 -- first tree that is (>=) x, if any.
-score :: Grid -> (Int, Int) -> Int -> Direction -> Int
-score grid (i, j) x d =
+score :: Grid -> Vec2 Int -> Int -> Direction -> Int
+score grid pos x d =
   length $ lower <> take 1 rest
  where
-  (lower, rest) = span (< x) $ scan grid (i, j) d
+  (lower, rest) = span (< x) $ scan grid pos d
 
 -- | Take until we hit the first 'Nothing'
 takeWhileJust :: [Maybe a] -> [a]
@@ -75,6 +76,6 @@ toGrid :: [[Int]] -> Grid
 toGrid xxs = HashMap.fromList $ do
   (i, xs) <- zip [0..] xxs
   (j, x) <- zip [0..] xs
-  pure ((i, j), x)
+  pure (Vec2 j i, x)
 
-type Grid = HashMap (Int, Int) Int
+type Grid = HashMap (Vec2 Int) Int
